04d15d6f5e8bb1fbd00e103b65b183869264d6ed
vengrasl <vengrasl@gmail.com>
2023-01-02 11:02:59 +0200

    task9

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,3 +1,4 @@
+
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Naudodami modulius sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
