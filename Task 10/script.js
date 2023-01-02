b3bf1e47f8bd3380d5361f0a9bb9f21a8dec6d6a
vengrasl <vengrasl@gmail.com>
2023-01-02 11:11:41 +0200

    10 uzduotis

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,4 +1,8 @@
-
+import {one, two, three, four, five} from './modules/numbers/numbers.js'
+import {composition} from './modules/math/composition.js'
+import {division} from './modules/math/division.js'
+import {substraction} from './modules/math/subtraction.js'
+import { multiplication } from './modules/math/multiplication.js'
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Naudodami modulius sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
