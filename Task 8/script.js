260d4837417c335bf092aa4ca0e021af42393eeb
vengrasl <vengrasl@gmail.com>
2023-01-02 11:03:01 +0200

    task9

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,5 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,22 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+
+class Movie {
+  constructor(title, director, budget){
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive(){
+    if (this.budget > 100000000){
+      return true;
+    } else{
+      return false;
+    }
+  }
+}
+
+let movie = new Movie ('filmo pavadinimas', 'režisierius', 10000000000);
+console.log(movie.wasExpensive())
