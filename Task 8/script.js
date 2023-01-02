b6bc53ab9d6f0ec022ee16f2b490bbe0b11f819d
vengrasl <vengrasl@gmail.com>
2023-01-02 11:22:31 +0200

    8 uzduotis

--- Task 8/script.js
+++ Task 8/script.js
@@ -7,3 +7,28 @@ division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
 
+class Calculator {
+  constructor(numberOne, numberTwo){
+    this.numberOne = numberOne;
+    this.numberTwo = numberTwo;
+  }
+  sum(){
+    return this.numberOne + this.numberTwo;
+  }
+  subtraction(){
+    return this.numberOne - this.numberTwo;
+  }
+  multiplication(){
+    return this.numberOne * this.numberTwo;
+  }
+  division(){
+    return this.numberOne / this.numberTwo;
+  }
+}
+
+let calculator = new Calculator(1, 2);
+
+console.log(calculator.sum());
+console.log(calculator.subtraction());
+console.log(calculator.multiplication());
+console.log(calculator.division());
